e49baa97349fbf6e7f8174a49cf86390cc497dda
Lê Bình Phương <69758987+BinhPhuongIT@users.noreply.github.com>
2022-04-28 16:50:27 +0700

    Update run.cmd

--- run.cmd
+++ run.cmd
@@ -1,2 +1,1 @@
-powershell.exe -ExecutionPolicy Bypass -File \\10.112.8.8\MySoftware\Utilities\PowerShell\Install_OpenSSH_Windows.ps1
-pause
+powershell -ExecutionPolicy Bypass -File .\Install_OpenSSH_Windows.ps1
