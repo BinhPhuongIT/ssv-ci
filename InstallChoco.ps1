9928a09336989609da3e78470bc13d45f829701c
Lê Bình Phương <69758987+BinhPhuongIT@users.noreply.github.com>
2022-04-28 16:57:38 +0700

    Delete InstallChoco.ps1

--- InstallChoco.ps1
+++
@@ -1,5 +0,0 @@
-﻿Get-ExecutionPolicy
-Set-ExecutionPolicy Bypass -Scope Process
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-choco upgrade chocolatey
-shutdown -r -t 0
