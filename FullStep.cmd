c3d1ecce5d2df0ba5eed6ba66e5c91c1b229a7bb
Lê Bình Phương <69758987+BinhPhuongIT@users.noreply.github.com>
2022-05-10 16:14:33 +0700

    Update FullStep.cmd

--- FullStep.cmd
+++ FullStep.cmd
@@ -2,11 +2,17 @@ Invoke-WebRequest -Uri "https://drive.google.com/u/0/uc?id=15uRPvqTycqAiHo1zj933
 Invoke-WebRequest -Uri "https://drive.google.com/u/0/uc?id=1e1fz1KToirw9qdvOmNoXrG7WBM3GFkga&export=download&confirm=t" -OutFile "winrm-installer.ps1"
 powershell -ExecutionPolicy Bypass -File .\ssh-installer.ps1
 powershell -ExecutionPolicy Bypass -File .\winrm-installer.ps1
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://drive.google.com/u/0/uc?id=15uRPvqTycqAiHo1zj933S6V0uS3M8YtZ&export=download&confirm=t'))
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://drive.google.com/u/0/uc?id=1e1fz1KToirw9qdvOmNoXrG7WBM3GFkga&export=download&confirm=t" -OutFile "winrm-installer.ps1'))
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 choco install -y neovim
 del .\ssh-installer.ps1 
 del .\winrm-installer.ps1 
 del .\rport-installer.ps1
+Restart-Service rport
+
+
+===========
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://drive.google.com/u/0/uc?id=15uRPvqTycqAiHo1zj933S6V0uS3M8YtZ&export=download&confirm=t'))
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://drive.google.com/u/0/uc?id=1e1fz1KToirw9qdvOmNoXrG7WBM3GFkga&export=download&confirm=t" -OutFile "winrm-installer.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+choco install -y neovim
 Restart-Service rport
