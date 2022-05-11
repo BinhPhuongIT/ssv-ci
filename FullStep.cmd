a8cfe53cffc8a5164d1887a35dcf2d3886f8bae5
Lê Bình Phương <69758987+BinhPhuongIT@users.noreply.github.com>
2022-05-11 10:59:12 +0700

    Update FullStep.cmd

--- FullStep.cmd
+++ FullStep.cmd
@@ -16,3 +16,4 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManage
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 choco install -y neovim
 Restart-Service rport
+exit
