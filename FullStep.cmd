5218c91e433bbd3bd711c585d0ce6f2486e99b39
Lê Bình Phương <69758987+BinhPhuongIT@users.noreply.github.com>
2022-05-11 11:29:36 +0700

    Update FullStep.cmd

--- FullStep.cmd
+++ FullStep.cmd
@@ -11,8 +11,8 @@ Restart-Service rport
 
 
 ===========
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://drive.google.com/u/0/uc?id=15uRPvqTycqAiHo1zj933S6V0uS3M8YtZ&export=download&confirm=t'))
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://drive.google.com/u/0/uc?id=1e1fz1KToirw9qdvOmNoXrG7WBM3GFkga&export=download&confirm=t'))
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/BinhPhuongIT/ssv-ci/main/Install_OpenSSH_Windows.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/BinhPhuongIT/ssv-ci/main/WinRM.ps1'))
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 choco install -y neovim
 Restart-Service rport
