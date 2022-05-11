d2ec6bc390883e8ea1777d4337deae3d41a9a692
Lê Bình Phương <69758987+BinhPhuongIT@users.noreply.github.com>
2022-05-11 11:27:59 +0700

    Add files via upload

---
+++ WinRM.ps1
@@ -0,0 +1,13 @@
+﻿Enable-PSRemoting -SkipNetworkProfileCheck -Force
+Set-NetConnectionProfile -NetworkCategory Private
+
+WinRM quickconfig
+
+
+winrm set winrm/config/client/auth '@{Basic="true"}'
+winrm set winrm/config/service/auth '@{Basic="true"}'
+
+Set-Item -Path WSMan:\localhost\Service\Auth\Basic -Value $true
+
+winrm set winrm/config/service/auth '@{Basic="true"}'
+winrm set winrm/config/service '@{AllowUnencrypted="true"}'
