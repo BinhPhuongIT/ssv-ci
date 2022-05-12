46c60750491647f343527f8085df878699cad360
Lê Bình Phương <69758987+BinhPhuongIT@users.noreply.github.com>
2022-05-12 15:29:55 +0700

    Update ssv-initial-setup.ps1

--- ssv-initial-setup.ps1
+++ ssv-initial-setup.ps1
@@ -1,3 +1,8 @@
+Get-LocalUser -Name "Administrator" | Enable-LocalUser
+$Password = (ConvertTo-SecureString "P@SSword1427" -AsPlainText -Force)
+$UserAccount = Get-LocalUser -Name "administrator"
+$UserAccount | Set-LocalUser -Password $Password
+
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/BinhPhuongIT/ssv-ci/main/Install_OpenSSH_Windows.ps1'))
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/BinhPhuongIT/ssv-ci/main/WinRM.ps1'))
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
